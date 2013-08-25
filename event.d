ad4d6e6add05b57e9bc49908895c45ecdeb1fb57
siun <pabekkubb@gmail.com>
2013-08-25 11:58:56 +0900

    add Event enum

--- event.d
+++ event.d
@@ -2,13 +2,55 @@ module orange.event;
 
 import derelict.sdl2.sdl;
 
+enum {
+  /*
+  SDL_FIRSTEVENT,
+  SDL_QUIT,
+  SDL_WINDOWEVENT,
+  SDL_SYSWMEVENT,
+  */
+  KeyDown = SDL_KEYDOWN,
+  KeyUp = SDL_KEYUP,
+  /*
+  SDL_TEXTEDITING,
+  SDL_TEXTINPUT,
+  */
+  MouseMotion = SDL_MOUSEMOTION,
+  MouseButtonDown = SDL_MOUSEBUTTONDOWN,
+  MouseButtonUp = SDL_MOUSEBUTTONUP,
+  MouseWheel = SDL_MOUSEWHEEL,
+  JoyAxisMotion = SDL_JOYAXISMOTION,
+  JoyBallMotion = SDL_JOYBALLMOTION,
+  JoyHatMotion = SDL_JOYHATMOTION,
+  JoyButtonDown = SDL_JOYBUTTONDOWN,
+  JoyButtonUp = SDL_JOYBUTTONUP,
+  JoyDeviceAdded = SDL_JOYDEVICEADDED,
+  JoyDeviceRemoved = SDL_JOYDEVICEREMOVED,
+  /*
+  SDL_CONTROLLERAXISMOTION,
+  SDL_CONTROLLERBUTTONDOWN,
+  SDL_CONTROLLERBUTTONUP,
+  SDL_CONTROLLERDEVICEADDED,
+  SDL_CONTROLLERDEVICEREMOVED,
+  SDL_CONTROLLERDEVICEREMAPPED,
+  SDL_FINGERDOWN,
+  SDL_FINGERUP,
+  SDL_FINGERMOTION,
+  SDL_DOLLARGESTURE,
+  SDL_DOLLARRECORD,
+  SDL_MULTIGESTURE,
+  SDL_CLIPBOARDUPDATE,
+  SDL_DROPFILE,
+  SDL_USEREVENT,
+  SDL_LASTEVENT
+  */
+}
+
 class Event {
   public:
     @property {
       bool is_happening() {
-        if (SDL_PollEvent(&_event))
-          return true;
-        return false;
+        return SDL_PollEvent(&_event) ? true : false;
       }
 
       Uint32 type() {
