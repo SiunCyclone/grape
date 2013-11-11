0878a5debae7d3236bba6c722f06e267b50ccd87
siun <pabekkubb@gmail.com>
2013-11-12 00:56:30 +0900

    update build.d

--- build/build.d
+++ build/build.d
@@ -8,29 +8,41 @@ void main() {
 }
 
 void buildDerelict3() {
-  string[] commands;
+  try {
+    string[] commands;
 
-  string directory = "../Derelict3/build";
-  commands ~= "git submodule init";
-  commands ~= "git submodule update";
-  commands ~= "dmd -of" ~ directory ~ "/build " ~ directory ~ "/build.d";
-  commands ~= directory ~ "/build";
-  
-  execute(commands);
+    string directory = "../Derelict3/build";
+    commands ~= "git submodule init";
+    commands ~= "git submodule update";
+    commands ~= "dmd -of" ~ directory ~ "/build " ~ directory ~ "/build.d";
+    commands ~= directory ~ "/build";
+    
+    execute(commands);
+  }
+
+  catch (Exception e) {
+    writeln("buildDerelict3() failed");
+  }
 }
 
 void buildOrange() {
-  string[] commands;
+  try {
+    string[] commands;
 
-	string target = "-of../lib/libOrange ";
-	string ops = "-lib -O -release -inline ";
-	string libs = "-I../Derelict3/import -L-L../Derelict3/lib/dmd ";
-	string sources = "";
-	foreach (file; dirEntries("../import/orange", "*.d", SpanMode.shallow))
-		sources ~= file ~ " ";
-	commands ~= "dmd " ~ ops ~ target ~ sources ~ libs;
+    string target = "-of../lib/libOrange ";
+    string ops = "-lib -O -release -inline ";
+    string libs = "-I../Derelict3/import -L-L../Derelict3/lib/dmd ";
+    string sources = "";
+    foreach (file; dirEntries("../import/orange", "*.d", SpanMode.shallow))
+      sources ~= file ~ " ";
+    commands ~= "dmd " ~ ops ~ target ~ sources ~ libs;
 
-  execute(commands);
+    execute(commands);
+  }
+
+  catch (Exception e) {
+    writeln("buildOrange() failed");
+  }
 }
 
 void execute(string[] commands) {
