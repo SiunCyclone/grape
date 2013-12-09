b858dfc2b3e6c4aa69edb8c1020dc1bad6f41b3b
siun <pabekkubb@gmail.com>
2013-12-09 20:31:06 +0900

    Impl Material and ColorMaterial

--- import/grape/material.d
+++ import/grape/material.d
@@ -3,28 +3,77 @@ module grape.material;
 import std.variant;
 import std.conv;
 import std.stdio;
+import grape.shader;
 
 class Material {
-  alias DataType = Algebraic!(int[], bool, string);
+  alias ParamType = Algebraic!(int[], bool, string);
 
   public:
     this(T...)(T params) {
+      init();
       set_param(params);
     }
 
-  private:
+  protected:
+    void init() {
+      _params["vertexShader"] = "vShader";
+      _params["fragmentShader"] = "fShader";
+    }
+
     void set_param(T...)(T params) {
       static if (params.length) {
         auto key = to!string(params[0]);
+        assert(key in _params, "Wrong material parameter's key named \"" ~ key ~ "\"");
         auto value = params[1];
 
-        hash[key] = value;
+        _params[key] = value;
         set_param(params[2..$]);
       } else {
         // TODO
       }
     }
 
-    DataType[string] hash; 
+    void create_program(in string vertexShaderSource, in string fragmentShaderSource) {
+      Shader vs = new Shader(ShaderType.Vertex, vertexShaderSource);
+      Shader fs = new Shader(ShaderType.Fragment, fragmentShaderSource);
+      _program = new ShaderProgram(vs, fs);
+    }
+
+    ParamType[string] _params;
+    ShaderProgram _program;
+}
+
+class ColorMaterial : Material {
+  public:
+    this(T...)(T params) {
+      super(params);
+
+      immutable vertexShaderSource = q{
+        attribute vec3 position;
+        attribute vec4 color;
+        varying vec4 vColor;
+
+        void main() {
+          vColor = color;
+          gl_Position = vec4(position, 1.0);
+        }
+      };
+
+      immutable fragmentShaderSource = q{
+        varying vec4 vColor;
+
+        void main() {
+          gl_FragColor = vColor;
+        }
+      };
+
+      create_program(vertexShaderSource, fragmentShaderSource);
+    }
+
+  protected:
+    override void init() {
+      _params["color"] = [ 255, 255, 255 ];
+      _params["wireframe"] = false;
+    }
 }
 
